4acc9de63887c22e6e78994d0dac7825fb74b5d6
Alysson Tormen <alyssontccruz@gmail.com>
2025-07-05 12:31:03 -0300

    feat: added header controller

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-(--screen-minus-header)">
+      <h1 className=" font-bold text-4xl">404</h1>
+      <p className="">Página não encontrada</p>
+    </div>
+  );
+}
---
+++ src/features/RootPage/controller/HeaderController.ts
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+export function useHeaderController() {
+  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = (e?: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e && e.key !== "Enter") return;
+    if (searchQuery.trim() !== "") {
+      navigate(`/search?query=${encodeURIComponent(searchQuery)}`);
+    }
+  };
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+
+  return {
+    handleLogoClick,
+    handleLoginClick,
+    handleRegisterClick,
+    handleSearchChange,
+    handleSearch,
+  };
+}
