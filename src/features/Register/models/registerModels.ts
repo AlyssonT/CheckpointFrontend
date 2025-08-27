9df6b424552a14bd98756327e6fd622483705ac8
Alysson Cruz <alyssontccruz@gmail.com>
2025-08-27 19:53:37 -0300

    hotfix: validation password message

--- src/features/Register/models/registerModels.ts
+++ src/features/Register/models/registerModels.ts
@@ -1,11 +1,14 @@
 import { z } from "zod/v4";
-import { INVALID_EMAIL_MESSAGE } from "../../../utils/validationMessages";
+import {
+  INVALID_EMAIL_MESSAGE,
+  INVALID_PASSWORD_MESSAGE,
+} from "../../../utils/validationMessages";
 
 export const registerSchema = z
   .object({
     name: z.string().min(3, "Username must have at least 6 characters."),
     email: z.email(INVALID_EMAIL_MESSAGE),
-    password: z.string().min(6, INVALID_EMAIL_MESSAGE),
+    password: z.string().min(6, INVALID_PASSWORD_MESSAGE),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
