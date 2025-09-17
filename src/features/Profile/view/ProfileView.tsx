0ed91cbabaa5d21098d17387c24918ec7aa13ed7
Alysson Cruz <alyssontccruz@gmail.com>
2025-09-17 11:42:15 -0300

    feat: responsive profile

--- src/features/Profile/view/ProfileView.tsx
+++ src/features/Profile/view/ProfileView.tsx
@@ -6,8 +6,8 @@ export function ProfileView() {
   const controller = useProfileController();
 
   return (
-    <div className="w-full flex flex-col justify-center items-center">
-      <div className="max-w-(--standard-page) pt-12 flex flex-col gap-8">
+    <div className="w-full h-auto flex justify-center items-center">
+      <div className="w-(--standard-page) pt-12 flex flex-col gap-8">
         <ProfileUserCard
           {...controller}
           totalReviews={controller.reviewsData.totalItems}
