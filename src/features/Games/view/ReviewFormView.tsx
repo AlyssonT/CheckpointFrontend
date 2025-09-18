4696c40338f10ce534269d68450a719e85e7b6a9
AlyssonT <alyssontccruz@gmail.com>
2025-09-18 10:44:54 -0300

    feat: game page responsive

--- src/features/Games/view/ReviewFormView.tsx
+++ src/features/Games/view/ReviewFormView.tsx
@@ -31,40 +31,40 @@ export function ReviewFormView({
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      <Paper className="w-(--standard-page) flex flex-col gap-4">
+      <Paper className="w-19/20 md:w-(--standard-page) flex flex-col gap-4">
         <form
           className="flex flex-col gap-4 p-4"
           onSubmit={handleSubmit(isEdit ? onSubmitPut : onSubmitPost)}
           noValidate
         >
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-2">
-              <p className="text-xl font-bold">
-                {(isEdit ? "Edit" : "Submit") + " Review"}
-              </p>
-              <BiEdit size={24} className="text-secondary" />
-            </div>
-            <div className="flex gap-8 items-center mr-4">
-              <Select
-                name="status"
-                label="Status"
-                register={register}
-                error={formState.errors.status?.message}
-                options={statusOptions}
-              />
-              <Input
-                name="score"
-                label="Score"
-                type="number"
-                className="min-w-32.5"
-                autoComplete="off"
-                min={0}
-                max={100}
-                register={register}
-                error={formState.errors.score?.message}
-              />
-            </div>
+          <div className="flex items-center gap-2 mb-2">
+            <p className="text-xl font-bold">
+              {(isEdit ? "Edit" : "Submit") + " Review"}
+            </p>
+            <BiEdit size={24} className="text-secondary" />
           </div>
+
+          <div className="flex flex-col md:flex-row gap-4 md:gap-8 md:justify-end md:mr-4">
+            <Select
+              name="status"
+              label="Status"
+              register={register}
+              error={formState.errors.status?.message}
+              options={statusOptions}
+            />
+            <Input
+              name="score"
+              label="Score"
+              type="number"
+              className="min-w-32.5"
+              autoComplete="off"
+              min={0}
+              max={100}
+              register={register}
+              error={formState.errors.score?.message}
+            />
+          </div>
+
           <TextArea
             name="review"
             label="Review"
@@ -73,6 +73,7 @@ export function ReviewFormView({
             maxLength={500}
             error={formState.errors.review?.message}
           />
+
           <div className="flex justify-end mt-4 gap-4">
             {isEdit && (
               <Button onClick={onDeleteReview} variant="danger">
